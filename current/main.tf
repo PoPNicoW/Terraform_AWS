ee04b9110d710a2aced573c2297d86c81ef0158e
PoPNicoW <n.bouige@dimsi.fr>
2018-09-27 18:57:38 +0200

    exercise 2

---
+++ current/main.tf
@@ -0,0 +1,31 @@
+provider "aws" {}
+
+resource "aws_instance" "example" {
+  ami		= "ami-075b44448d2276521"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+	      nohup busybox httpd -f -p 8080 &
+              EOF
+
+  tags {
+    Name = "terraform-example"
+       }
+}
+
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+   }
+}
+
+
